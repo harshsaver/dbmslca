2184e9eb32b1e520a036ef68ebb6f3a1385ed264
Harsh Savergaonkar <harsh.savergaonkar@gmail.com>
2020-09-22 17:24:41 +0530

    Update calculate.php

--- calculate.php
+++ calculate.php
@@ -1,55 +1,39 @@
-<?php
+<?php 
 
-require_once 'DbConnect.php';
-$response = array();
+	require_once 'DbConnect.php';
+	$response = array();
 
-if (isTheseParametersAvailable(array(
-    'currID'
-)))
-{
+					if(isTheseParametersAvailable(array('currID'))){
 
-    $currID = $_POST['currID'];
-    $currSalary = 0;
-    $stmt = $conn->prepare("SELECT salary FROM Emp WHERE empno = ?");
-    $stmt->bind_param("i", $currID);
-    $stmt->execute();
-    $stmt->bind_result($currSalary);
-    $stmt->fetch();
-    $stmt->close();
-    if ($currSalary < 200000)
-    {
-        $response['error'] = false;
-        $response['Taxed Amount'] = "You are excempted from tax";
-    }
-    else if ($currSalary < 500000 && $currSalary >= 200000)
-    {
-        $response['error'] = false;
-        $response['Taxed Amount'] = $currSalary * (5 / 100);
-    }
-    else if ($currSalary > 500001 && $currSalary < 1000000)
-    {
-        $response['error'] = false;
-        $response['Taxed Amount'] = $currSalary * (20 / 100);
-    }
-    else if ($currSalary > 1000001)
-    {
-        $response['error'] = false;
-        $response['Taxed Amount'] = $currSalary * (30 / 100);
-    }
+						$currID = $_POST['currID'];
+						$currSalary = 0;
+						$stmt = $conn->prepare("SELECT salary FROM Emp WHERE empno = ?"); 
+							$stmt->bind_param("i",$currID);
+							$stmt->execute();
+							$stmt->bind_result($currSalary);
+							$stmt->fetch();
+							$stmt->close();
+					
 
+					if (!$mysqli->query("CALL calcTax('$currSalary')")) {
+    					echo "CALL failed: (" . $mysqli->errno . ") " . $mysqli->error;
+    					$response['error'] = true; 
+					$response['message'] = 'Fail'; 
+					}else{
+						$response['error'] = false; 
+							$response['message'] = 'Done'; 
+					}
+					
+				
 }
-echo json_encode($response);
-
-function isTheseParametersAvailable($params)
-{
-
-    foreach ($params as $param)
-    {
-        if (!isset($_POST[$param]))
-        {
-            return false;
-        }
-    }
-    return true;
-}
-
+				echo json_encode($response);
+	
+	function isTheseParametersAvailable($params){
+		
+		foreach($params as $param){
+			if(!isset($_POST[$param])){
+				return false; 
+			}
+		}
+		return true; 
+	}
